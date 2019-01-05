f11ffdcf27ef531f02c1fdd768d334d19c55590e
tynchtyk <tynchtykkaldybaev@gmail.com>
2019-01-05 15:51:55 +0600

    Descriptions are partly done

--- app/src/main/java/tynchtykbekkaldybaev/kettik/Description/Description.java
+++ app/src/main/java/tynchtykbekkaldybaev/kettik/Description/Description.java
@@ -1,5 +1,6 @@
 package tynchtykbekkaldybaev.kettik.Description;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -7,10 +8,15 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import tynchtykbekkaldybaev.kettik.Choose_Language;
+import tynchtykbekkaldybaev.kettik.MainActivity;
 import tynchtykbekkaldybaev.kettik.R;
 
 /**
@@ -20,6 +26,8 @@ import tynchtykbekkaldybaev.kettik.R;
 public class Description extends AppCompatActivity {
     private ViewPager viewPager;
     private BottomNavigationView bottomNavigationView;
+    private TextView next, back;
+    Intent intent = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +35,59 @@ public class Description extends AppCompatActivity {
         viewPager = findViewById(R.id.viewpager);
         bottomNavigationView = findViewById(R.id.appBarLayout);
 
+        next = findViewById(R.id.next);
+        back = findViewById(R.id.back);
+
+        next.setOnClickListener(mOnKeyClickListener);
+        back.setOnClickListener(mOnKeyClickListener);
+
+
+
         setupViewPager(viewPager, 3);
 
+
     }
+
+    private View.OnClickListener mOnKeyClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int currentFragment = viewPager.getCurrentItem();
+            if (v.equals(next)) {
+                if(currentFragment != 2) {
+                    viewPager.setCurrentItem(currentFragment + 1);
+                }
+                else {
+                    intent = new Intent(Description.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+            else if(v.equals(back)) {
+                if(currentFragment != 0) {
+                    viewPager.setCurrentItem(currentFragment - 1);
+                }
+                else {
+                    intent = new Intent(Description.this, Choose_Language.class);
+                    startActivity(intent);
+                }
+            }
+        }
+    };
 
     private void setupViewPager(ViewPager viewPager, int fragmentCount) {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
 
-        for(int i=0; i<fragmentCount; i++) {
-            Description1 tmp = new Description1();
-            viewPagerAdapter.addFragment(tmp);
-        }
+        Description1 tmp1 = new Description1();
+        viewPagerAdapter.addFragment(tmp1);
+
+        Description2 tmp2 = new Description2();
+        viewPagerAdapter.addFragment(tmp2);
+
+        Description3 tmp3 = new Description3();
+        viewPagerAdapter.addFragment(tmp3);
+
+
+
         viewPager.setAdapter(viewPagerAdapter);
 
     }
---
+++ app/src/main/java/tynchtykbekkaldybaev/kettik/Description/Description3.java
@@ -0,0 +1,30 @@
+package tynchtykbekkaldybaev.kettik.Description;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tynchtykbekkaldybaev.kettik.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Description3 extends Fragment {
+
+
+    public Description3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description3, container, false);
+    }
+
+}
