78bf061bfa4be1bbe095e4064c87aa0131070cd9
tynchtyk <tynchtykkaldybaev@gmail.com>
2019-01-22 14:38:03 +0600

    Desctrion was edited and navheader

--- app/src/main/java/tynchtykbekkaldybaev/kettik/Choose_Language.java
+++ app/src/main/java/tynchtykbekkaldybaev/kettik/Choose_Language.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import tynchtykbekkaldybaev.kettik.Description.Description;
@@ -13,7 +14,7 @@ import tynchtykbekkaldybaev.kettik.Description.Description;
  */
 
 public class Choose_Language extends AppCompatActivity {
-    private TextView kg,ru,en;
+    private Button kg,ru,en;
     Intent intent = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
