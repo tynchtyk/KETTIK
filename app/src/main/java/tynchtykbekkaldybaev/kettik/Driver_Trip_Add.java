c333a0ec5bf0bb032ce42c616e0002f49b176a2e
tynchtyk <tynchtykkaldybaev@gmail.com>
2019-01-31 20:42:40 +0600

    try add trip to server

--- app/src/main/java/tynchtykbekkaldybaev/kettik/Driver_Trip_Add.java
+++ app/src/main/java/tynchtykbekkaldybaev/kettik/Driver_Trip_Add.java
@@ -1,22 +1,42 @@
 package tynchtykbekkaldybaev.kettik;
 
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.DatePicker;
 import com.applandeo.materialcalendarview.builders.DatePickerBuilder;
 import com.applandeo.materialcalendarview.listeners.OnSelectDateListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.List;
 
@@ -25,20 +45,30 @@ import java.util.List;
  */
 
 public class Driver_Trip_Add extends AppCompatActivity {
-    private ImageView back;
     private TextView date;
+    private EditText from, where, price, quantity;
     private ImageView cal;
-    DatePicker datePicker;
-
     private TextView time;
+    private Button send;
+
     private ImageView timeImage;
-    TimePickerDialog.OnTimeSetListener mTimeSetListener;
+    private ImageView back;
+    private DatePicker datePicker;
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.driver_route_add);
 
         back = findViewById(R.id.back);
+        date = findViewById(R.id.date);
+        time = findViewById(R.id.time);
+        timeImage = findViewById(R.id.timeimage);
+        from = findViewById(R.id.from);
+        where = findViewById(R.id.where);
+        quantity = findViewById(R.id.quantity);
+        price = findViewById(R.id.price);
+        send = findViewById(R.id.add);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +77,6 @@ public class Driver_Trip_Add extends AppCompatActivity {
             }
         });
 
-        date = findViewById(R.id.date);
         Calendar calendar = Calendar.getInstance();
 
         DatePickerBuilder builder1 = new DatePickerBuilder(this,new OnSelectDateListener() {
@@ -74,7 +103,6 @@ public class Driver_Trip_Add extends AppCompatActivity {
                 .pickerType(CalendarView.ONE_DAY_PICKER);
         datePicker = builder1.build();
 
-        cal = findViewById(R.id.calendar);
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,8 +111,6 @@ public class Driver_Trip_Add extends AppCompatActivity {
         });
 
 
-        time = findViewById(R.id.time);
-        timeImage = findViewById(R.id.timeimage);
 
         mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
@@ -101,6 +127,30 @@ public class Driver_Trip_Add extends AppCompatActivity {
             }
         });
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    collect_data();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    public void collect_data() throws JSONException {
+        JSONObject data = new JSONObject();
+        data.put("from", from.getText().toString());
+        data.put("where", where.getText().toString());
+        data.put("date", date.getText().toString());
+        data.put("time", time.getText().toString());
+        data.put("price", price.getText().toString());
+        data.put("quantity", quantity.getText().toString());
+
+        requestThread task = new requestThread();
+        task.execute(String.valueOf(data.toString()));
     }
 
     public void showTimePickerDialog(View view) {
@@ -119,5 +169,100 @@ public class Driver_Trip_Add extends AppCompatActivity {
 
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
+    }
+
+    public class requestThread extends AsyncTask<String,Void,String> {
+        ProgressDialog progressDialog;
+
+        private String submitURL =
+                "http://81.214.24.77:7777/api/trips?fbclid=IwAR3cDiiEDJ3nzUcpOD3LkaPxWvwi9232rpHlLNARny1eirQNu4cIYLy0his";
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            return sendData(strings[0]);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(Driver_Trip_Add.this);
+            progressDialog.setMessage("Отправка данных...");
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
+            // this is expecting a response code to be sent from your server upon receiving the POST data
+
+        }
+
+        public String sendData(String data){
+            if ( checkConnection() ) {
+                String JsonResponse = null;
+                Log.e("SEND", data);
+                HttpURLConnection urlConnection = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL(submitURL);
+
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+
+                    OutputStream os = urlConnection.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(data);
+                    writer.flush();
+
+                    reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while ((line = reader.readLine()) != null) {
+                        // Append server response in string
+                        sb.append(line + "\n");
+                    }
+
+                    Log.e("OTVET", sb.toString());
+                    writer.close();
+                    os.close();
+                    urlConnection.connect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return "";
+                }
+
+                return JsonResponse;
+            } else {
+                return "";
+            }
+        }
+    }
+
+    public boolean checkConnection(){
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+        try
+        {
+            ConnectivityManager cm = (ConnectivityManager) Driver_Trip_Add.this.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+            for (NetworkInfo ni : netInfo) {
+                if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                    if (ni.isConnected())
+                        haveConnectedWifi = true;
+                if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                    if (ni.isConnected())
+                        haveConnectedMobile = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return (haveConnectedWifi || haveConnectedMobile);
     }
 }
